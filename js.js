9ecd4566ef0fe34226b0a8ea7cb8bede857fec25
noshacode <nuhasaleh2010@gmail.com>
2022-01-07 00:15:20 +0100

    fix css styling

--- js.js
+++ js.js
@@ -1,38 +1,17 @@
-
-$(".button").click(function(){
+$("#form").submit(function (e) {
+    e.preventDefault();
     $('#result').empty()
-   const newInput = $( "input" ).val();
-    console.log(newInput)
-    
-    $.get(`https://g.tenor.com/v1/search?q=${newInput}&key=GCFK5W3RG890&limit=8`, function(data, status){
+    const newInput = $("#search").val();
+    const number = $("#limit").val() || 10;
+
+    $.get(`https://g.tenor.com/v1/search?q=${newInput}&key=GCFK5W3RG890&limit=${number}`, function (data) {
         showPic(data);
     })
-    // let http = new XMLHttpRequest();
-    // http.open('GET', `https://api.tenor.com/v1/search?q=${newInput}&key=LIVDSRZULELA&limit=8`);
-    // http.responseType = 'json';
-    
-    // http.onreadystatechange = function() {
-    //     if (http.readyState === 4 && http.status === 200) {
-    //         showPic(http.response);
-    //     }
-    // };
-    // http.send();
 })
 
-
-
-
-
-
-
-
-     
-function showPic(arr){
-    arr.results.forEach(gif => {
-        console.log(gif)
-        const newTag = $('<div class="container"></div>')
-        const gifImage = $(`<img class="gif-image" src=${gif.media[0].gif.url}>`)
-        newTag.append(gifImage)
-        $('#result').append(newTag)    
+function showPic(obj) {
+    obj.results.forEach(result => {
+        const gifImage = $(`<img class="gif-image" src=${result.media[0].gif.url}>`)
+        $('#result').append(gifImage)
     });
 }
