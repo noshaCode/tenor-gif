86893717420a49c4a9c374b8a348fcb912d3fb07
noshacode <nuhasaleh2010@gmail.com>
2022-01-06 18:03:09 +0100

    second commit

--- js.js
+++ js.js
@@ -1,20 +1,22 @@
 
-
 $(".button").click(function(){
     $('#result').empty()
    const newInput = $( "input" ).val();
     console.log(newInput)
-    let http = new XMLHttpRequest();
-    let search = "birds"
-    http.open('GET', `https://api.tenor.com/v1/search?q=${newInput}&key=LIVDSRZULELA&limit=8`);
-    http.responseType = 'json';
     
-    http.onreadystatechange = function() {
-        if (http.readyState === 4 && http.status === 200) {
-            showPic(http.response);
-        }
-    };
-    http.send();
+    $.get(`https://g.tenor.com/v1/search?q=${newInput}&key=GCFK5W3RG890&limit=8`, function(data, status){
+        showPic(data);
+    })
+    // let http = new XMLHttpRequest();
+    // http.open('GET', `https://api.tenor.com/v1/search?q=${newInput}&key=LIVDSRZULELA&limit=8`);
+    // http.responseType = 'json';
+    
+    // http.onreadystatechange = function() {
+    //     if (http.readyState === 4 && http.status === 200) {
+    //         showPic(http.response);
+    //     }
+    // };
+    // http.send();
 })
 
 
@@ -27,6 +29,7 @@ $(".button").click(function(){
      
 function showPic(arr){
     arr.results.forEach(gif => {
+        console.log(gif)
         const newTag = $('<div class="container"></div>')
         const gifImage = $(`<img class="gif-image" src=${gif.media[0].gif.url}>`)
         newTag.append(gifImage)
